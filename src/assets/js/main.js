35f5b37610c85688e23469bd4657ad6cf39ac702
Malikhai <101726@glr.nl>
2025-10-07 15:00:39 +0000

    naving

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,10 +1,14 @@
+import { gsap } from '/gsap-public/esm/index.js';
+import { SplitText } from '/gsap-public/esm/SplitText.js';
+
 function page() {
 const nameElement = document.getElementById("namel");
+
 const targetText = "Malikhai Felix";
 const initialText = 'abcdefghijklmn';
 const charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&* ";
 
-const charAnimationDuration = 200;
+const charAnimationDuration = 50;
 const charChangeInterval = 12.5;
 const delayBetweenChars = 50;
 
@@ -43,7 +47,7 @@ const startLetterByLetterAnimation = () => {
 
 const nameLoad = () => {
     nameElement.textContent = initialText;
-    setTimeout(startLetterByLetterAnimation, 500);
+    setTimeout(startLetterByLetterAnimation, 100);
 };
 
 document.addEventListener("DOMContentLoaded", () => setTimeout(startLetterByLetterAnimation, 1000));
@@ -51,3 +55,31 @@ document.addEventListener("DOMContentLoaded", () => setTimeout(startLetterByLett
 nameElement.addEventListener("click", nameLoad);
 };
 page();
+
+gsap.set("div", { opacity: 1 });
+let split = SplitText.create("#wel", { type: "chars" });
+gsap.registerPlugin(SplitText);
+gsap.from(split.chars, {
+  y: 400,
+  autoAlpha: 0,
+  stagger: 0.05,
+  duration: 0.1,  // Duration in seconds (1500ms)
+});
+
+const navPages = [
+    {button: document.getElementById("namel"), content: document.getElementById("box1")},
+    {button: document.getElementById("abo"), content: document.getElementById("box2")},
+    {button: document.getElementById("proj"), content: document.getElementById("box3")},
+    {button: document.getElementById("con"), content: document.getElementById("box4")}
+];
+
+
+for (let i = 0; i < navPages.length; i++) {
+    navPages[i].button.addEventListener("click", () => {
+        navPages.forEach(page => {
+            page.button.style.color = "#b5b5b5";
+        });
+        navPages[i].button.style.color = "#ffffff";
+        console.log("Active button:", navPages[i].button.textContent);
+    });
+}
